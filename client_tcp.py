f94e989b257ae53490f7c1ce0f2a67dc445903e7
chennakt9 <chenna.kesava@iitgn.ac.in>
2020-11-20 08:59:24 +0530

    first commit

---
+++ client_tcp.py
@@ -0,0 +1,35 @@
+import socket
+import time
+import os
+
+
+start_time = time.time()
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((socket.gethostname(), 12345))
+
+
+
+file  = open('Downloads/')
+while True:
+    data = client.recv(2**17)
+    if len(data) <= 0:
+        break
+
+    # print(data)
+    dcdmsg = data.decode("utf-8", "ignore")
+
+    print(len(dcdmsg))
+    
+    file.write(dcdmsg)
+
+client.close()
+
+end_time = time.time()
+
+time_diff = end_time - start_time
+
+print(f"time diff in seconds : {round(time_diff,2)} s")
+
+
+
---
+++ server_tcp.py
@@ -0,0 +1,56 @@
+import socket
+import time
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# server.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY, True)  #For Disabling Nagle's Algorithm
+
+
+server.bind((socket.gethostname(), 12345))
+
+
+
+server.listen(1)
+
+
+BUFFER_SIZE = 32
+
+
+
+
+# FC text2TCP8224.txt ../Texts/text2.txt
+
+while True:
+
+    clientsocket, address = server.accept()
+
+    print(f"Connection from {address} has been established.")
+
+    
+    book_name = clientsocket.recv(128).decode('utf-8')
+
+    print("book name : ",book_name)
+
+
+    file = open(f'../Texts/{book_name}.txt', 'r', encoding="utf-8")
+    
+    
+
+
+    while True:
+
+        # time.sleep(1/(10**9))
+
+        data = file.read(BUFFER_SIZE)
+
+        # print(data)
+        
+
+        if not data:
+            file.close()
+            break
+        
+        clientsocket.send( bytes(data,"utf-8"))
+        
+
+    clientsocket.close()
